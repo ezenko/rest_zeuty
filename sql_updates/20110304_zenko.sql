2ef4ce57d25feffe16936aa7f7aad913e2370be1
Yauhen Zenko <e.zenko@bylectric.com>
2011-03-04 10:09:02 +0200

    Additional fields

---
+++ sql_updates/20110304_zenko.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE `re_user_rent_pays` ADD `days` VARCHAR( 255 ) NULL ,
+ADD `hotel` VARCHAR( 255 ) NULL ,
+ADD `route` TEXT NULL ,
+ADD `facilities` VARCHAR( 255 ) NULL ,
+ADD `meals` VARCHAR( 255 ) NULL ;
