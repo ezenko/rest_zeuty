8f97e7d6834ac36e2565d0e254dcfdb92bdeaac6
Yauhen Zenko <e.zenko@bylectric.com>
2011-05-17 12:02:41 +0300

    Index page fixes

--- include/functions_index.php
+++ include/functions_index.php
@@ -4505,11 +4505,17 @@ function GetCityWeather ($gismeteo_id) {
   }
   
   if (is_file(dirname(__FILE__) . '/../cache/' . $file . '.cache')) {
-    $xmlObj = @simplexml_load_file(dirname(__FILE__) . '/../cache/' . $file . '.cache');
+    $t = file_get_contents(dirname(__FILE__) . '/../cache/' . $file . '.cache');
+    if ($t) {
+      $xmlObj = @simplexml_load_file(dirname(__FILE__) . '/../cache/' . $file . '.cache');
+    }
   } else {
     if ($file) {
-      file_put_contents(dirname(__FILE__) . '/../cache/' . $file . '.cache', file_get_contents('http://informer.gismeteo.ru/xml/' . $gismeteo_id . '.xml'));
-      $xmlObj = @simplexml_load_file(dirname(__FILE__) . '/../cache/' . $file . '.cache');
+      $t = file_get_contents('http://informer.gismeteo.ru/xml/' . $gismeteo_id . '.xml');
+      if ($t) { 
+        file_put_contents(dirname(__FILE__) . '/../cache/' . $file . '.cache', $t);
+        $xmlObj = @simplexml_load_file(dirname(__FILE__) . '/../cache/' . $file . '.cache');
+      }
     }
   }
   
